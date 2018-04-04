d72e7113e8a7eda81cebc5d38d3f3f389d112de0
Sharath Mekottil <sharath.m@tavant.com>
2018-04-04 13:10:46 +0530

    Added more testcases

--- test/com/learn/algorithm/sort/SortTest.java
+++ test/com/learn/algorithm/sort/SortTest.java
@@ -12,12 +12,24 @@ class SortTest {
 	private int []unsorted_non_repeated_input = {3,4,2,1,5,77,66,33,87};
 	private int []unsorted_non_repeated_output = {1,2,3,4,5,33,66,77,87};
 	
+	private int []singleElementInput = {3};
+	private int []singleElementOutput = {3};
+	
+	
+	private int []unsorted_repeated_input = {3,4,2,1,3,4,2,1};
+	private int []unsorted_repeated_output = {1,1,2,2,3,3,4,4};
+	
+	
+	private int []null_input = {};
+	private int []null_sorted = {};
+	
+	
 	private List<Sort> sorts= new ArrayList<>();
 	
 	{
-//		sorts.add(new BubbleSort());
-//		sorts.add(new SelectionSort());		
-//		sorts.add(new InsertionSort());
+		sorts.add(new BubbleSort());
+		sorts.add(new SelectionSort());		
+		sorts.add(new InsertionSort());
 		sorts.add(new MergeSort());
 	}
 	
@@ -28,6 +40,32 @@ class SortTest {
 			Assert.assertArrayEquals(output, unsorted_non_repeated_output);
 		}
 	}
+	
+	
+	@Test
+	void testSingleElement() {
+		for (Sort sort : sorts) {
+			int[] output = sort.sort(singleElementInput.clone());
+			Assert.assertArrayEquals(output, singleElementOutput);
+		}
+	}
+	
+	@Test
+	void testUnsortedRepeated() {
+		for (Sort sort : sorts) {
+			int[] output = sort.sort(unsorted_repeated_input.clone());
+			Assert.assertArrayEquals(output, unsorted_repeated_output);
+		}
+	}
+	
+	@Test
+	void testNull() {
+		for (Sort sort : sorts) {
+			int[] output = sort.sort(null_input.clone());
+			Assert.assertArrayEquals(output, null_sorted);
+		}
+	}
+	
 	
 	
 
